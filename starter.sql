c5b50707772306dab92cc882d509c40e310c0e5c
1U25CS032- Gunasekaran T <g8208003@gmail.com>
2026-09-03 19:21:26 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,26 +1,24 @@
--- StudentDetails View Assignment
 
--- Create Department table
 CREATE TABLE Department (
     DepartmentID INT PRIMARY KEY,
     DepartmentName VARCHAR(30)
 );
+desc Department;
 
--- Create Student table
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
     StudentName VARCHAR(30),
     DepartmentID INT,
     FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
 );
+desc Student;
 
--- Create Course table
 CREATE TABLE Course (
     CourseID INT PRIMARY KEY,
     CourseName VARCHAR(30)
 );
+desc Course;
 
--- Create Enrollment table
 CREATE TABLE Enrollment (
     EnrollmentID INT PRIMARY KEY,
     StudentID INT,
@@ -28,42 +26,43 @@ CREATE TABLE Enrollment (
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
     FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
 );
+desc Enrollment;
 
--- Insert Department values
 INSERT INTO Department VALUES
 (1, 'CSE'),
 (2, 'IT'),
 (3, 'ECE');
 
--- Insert Student values
 INSERT INTO Student VALUES
 (101, 'Arun', 1),
 (102, 'Divya', 2),
 (103, 'Karthik', 1);
 
--- Insert Course values
+
 INSERT INTO Course VALUES
 (201, 'DBMS'),
 (202, 'Java'),
 (203, 'Python');
 
--- Insert Enrollment values
+
 INSERT INTO Enrollment VALUES
 (1, 101, 201),
 (2, 101, 202),
 (3, 102, 203),
 (4, 103, 201);
 
--- =====================================================
--- STUDENT TASK
--- Create a view named StudentDetails.
---
--- The view must display:
--- StudentName
--- CourseName
--- DepartmentName
---
--- Write your CREATE VIEW statement below.
--- =====================================================
 
--- YOUR CODE HERE
+CREATE VIEW StudentDetails AS
+SELECT
+    s.StudentName,
+    c.CourseName,
+    d.DepartmentName
+FROM Student s
+JOIN Enrollment e
+    ON s.StudentID = e.StudentID
+JOIN Course c
+    ON e.CourseID = c.CourseID
+JOIN Department d
+    ON s.DepartmentID = d.DepartmentID;
+
+SELECT * FROM StudentDetails;
